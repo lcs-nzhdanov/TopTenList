9a9b67dfba46370a3c443669650917a757e873f0
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-26 09:54:46 -0500

    Have done top ten list, printed 1st, 5th, last and element out of range

--- TopTenList/main.swift
+++ TopTenList/main.swift
@@ -7,5 +7,9 @@
 
 import Foundation
 
-print("Hello, World!")
+let TopTen = ["RTX 2060", "GTX 1080", "RTX 2070", "RTX 2070", "RTX 2080", "RTX 3060", "RTX 3070", "RTX 3080", "RTX 4060", "RTX 4070", "RTX 4080"]
 
+print(TopTen.first!)
+print(TopTen[5])
+print(TopTen.last!)
+print(TopTen[TopTen.count])
