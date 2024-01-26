bdd1111afd8c7f42aa28008d52b114541f1fdea2
Nikita Zhdanov <nzhdanov@lcs.on.ca>
2024-01-26 10:10:57 -0500

    completed exerceise 2 by printing out each element using for

--- TopTenList/main.swift
+++ TopTenList/main.swift
@@ -9,7 +9,18 @@ import Foundation
 
 let TopTen = ["RTX 2060", "GTX 1080", "RTX 2070", "RTX 2070", "RTX 2080", "RTX 3060", "RTX 3070", "RTX 3080", "RTX 4060", "RTX 4070", "RTX 4080"]
 
+//Divider Ex. 1
+print("\n-----------\nExercise 1:\n")
+
 print(TopTen.first!)
 print(TopTen[5])
 print(TopTen.last!)
-print(TopTen[TopTen.count])
+//print(TopTen[TopTen.count])
+
+
+//Divider Ex. 2
+print("\n-----------\nExercise 2:\n")
+
+for videoCard in TopTen {
+    print(videoCard)
+}
